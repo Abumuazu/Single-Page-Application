7a37008ab90f5c9865ff7e0d4bd0e7ec4938a59f
Decagon <e@Mzee.local>
2021-08-29 17:26:46 +0100

    consuming the api and redering on the forntend page

---
+++ script 5.20.40 PM/index.js
@@ -0,0 +1,69 @@
+
+
+const main = () => {
+
+    fetch("https://swapi.dev/api/people")
+    .then(response => {
+        if(!response.ok){ throw Error("Error")};
+       return response.json();
+    })
+    .then(objectData => { console.log(objectData.results)
+    
+const accessObjectData = objectData.results.map((element,index) => {
+  
+        return `  <div  class="container">
+
+         <div class="people">
+
+                <div class="box${index} box">
+                    <div class="bg"></div>
+                    <div class="details child1">
+                        <h1>Meet Our <br/> Models</h1>
+                        <button onclick = "toggleText('show${index}')" >${element.name}</button><br/>
+                      <div class= "house" style =" display: none" id ="show${index}">  <p class="API" >
+                      <ul class="type">
+                         <li> NAME: ${element.name}</li>
+                         <li> GENDER: ${element.gender}</li>
+                        <li>HEIGHT: ${element.height}</li>
+                      </ul>
+                        </p> </div>
+                        
+                    </div>
+
+                    <div class="illustration child2">
+                      <div style="position:relative" class="inner">
+                      <img style=" border-radius: 30px" width="100%" height ="99.94%" src="images/img${index}.jpg" alt="image">
+                    </div>
+                  </div>
+                </div>
+                  
+            </div> </div>`
+            
+    })
+
+    
+        
+
+
+        
+ document
+ .querySelector("#catch").insertAdjacentHTML("beforeend", accessObjectData)
+
+
+
+})
+
+    
+}
+main()
+
+
+
+
+const toggleText = (id) =>  {
+  let text = document.getElementById(id);
+    if (text.style.display === "none") { text.style.display = "block"; } 
+    else { text.style.display = "none"; }
+}
+
+
